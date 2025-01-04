e8044c9680f158c7b31edc369658c7fe58e56502
Taetea1 <xodus2079@naver.com>
2025-01-04 20:14:10 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,13 @@
+function solution(a, d, included) {
+    var answer = 0;
+    let arr=[];
+    for(let i=0;i<included.length;i++){
+        arr[i]=a+d*i;
+        if(included[i]===true){
+            answer+=arr[i];
+        } else{
+            continue;
+        }
+    }
+    return answer;
+}
