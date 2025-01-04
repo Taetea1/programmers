bd4addb86537d57a54efccae6ca2603737f102f2
Taetea1 <xodus2079@naver.com>
2025-01-04 22:33:26 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 27.10 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,11 +1,9 @@
 function solution(arr, queries) {
     let result=[];
-    for(let [s,e,k] of queries){
-        let num = arr.filter((v,i)=>(i>=s&&i<=e&&v>k)).sort((a,b)=>a-b)[0];
+    queries.forEach(([s,e,k])=>{
+        let temp=arr.filter((v,i)=>(i>=s&&i<=e&&arr[i]>k)).sort((a,b)=>a-b)[0];
         
-        result.push(num?num:-1);
-    }
-    
-    
+        result.push(temp?temp:-1);
+    });
     return result;
 }
