20aeb1e60bc47c79bb9caf229447a50e04785567
KwonTaeyeon <xodus2079@naver.com>
2025-02-17 14:33:20 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.14 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,7 +1,3 @@
 function solution(myString, pat) {
-    var answer = 0;
-    let a = myString.toLowerCase();
-    let b=pat.toLowerCase();
-    a.includes(b)? answer=1:answer=0;
-    return answer;
+    return +(myString.toLowerCase().includes(pat.toLowerCase()));
 }
